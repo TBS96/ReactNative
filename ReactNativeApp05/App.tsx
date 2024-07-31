44b5b7788dbde701dbb9d5696b3b11d9e6c125b7
TBS96 <prantik.g1996@gmail.com>
2024-07-31 16:23:55 +0530

    added logic to generatePasswordStr()

--- ReactNativeApp05/App.tsx
+++ ReactNativeApp05/App.tsx
@@ -4,12 +4,14 @@ import React, { useState } from 'react'
 // Form Validation
 import * as Yup from 'yup'
 
+
 const PasswordSchema = Yup.object().shape({
   passwordLength: Yup.number()
   .min(4, 'Should be min of 4 characters')
   .max(16, 'Should be max of 16 characters')
   .required('Length is required')
 })
+
 
 const App = () => {
 
@@ -20,7 +22,36 @@ const App = () => {
   const [numbers, setNumbers] = useState(false);
   const [symbols, setSymbols] = useState(false);
 
-  const generatePasswordStr = (passwordLength: string) => {}
+  const generatePasswordStr = (passwordLength: number) => {
+    let characterList = '';
+    const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const digitChars = '0123456789';
+    const specialChars = "~`!@#$%^&*()_+=-[]{}:;'<>,./";
+
+    if(upperCase)
+    {
+      characterList += upperCase;
+    }
+    if(lowerCase)
+    {
+      characterList += lowerCase;
+    }
+    if(numbers)
+    {
+      characterList += numbers;
+    }
+    if(symbols)
+    {
+      characterList += symbols;
+    }
+
+    const passwordResult = createPassword(characterList, passwordLength);
+
+    setPassword(passwordResult);
+    setIsGenerated(true);
+  }
+
 
   const createPassword = (characters: string, passwordLength: number) => {
 
@@ -35,6 +66,7 @@ const App = () => {
 
   }
 
+  
   const resetPasswordState = () => {}
 
   return (
