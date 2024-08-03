b4e9a722204f0e0853628a8164f22e5e29942f1c
TBS96 <prantik.g1996@gmail.com>
2024-08-03 20:06:42 +0530

    change bgcolor of whole bg including statusbar with clean logic, with the help of a btn(Touchable Opacity)
    Signed-off-by: TBS96 <prantik.g1996@gmail.com>

--- ReactNativeApp06/App.tsx
+++ ReactNativeApp06/App.tsx
@@ -1,14 +1,52 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [randomBg, setRandomBg] = useState('#ffffff');
+
+  const generateColor = () => {
+    const hexRange = '00123456789ABCDEF';
+    let color = '#';
+
+    for(let i = 0; i < 6; i++)
+    {
+      color += hexRange[Math.floor(Math.random() * 16)];
+    }
+    setRandomBg(color);
+  };
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <>
+      <StatusBar backgroundColor={randomBg} />
+      <View style={[styles.container, {backgroundColor: randomBg}]}>
+        <TouchableOpacity onPress={generateColor}>
+          <View style={styles.actionBtn}>
+            <Text style={styles.actionBtnTxt}>Press Me</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  actionBtn: {
+    borderRadius: 12,
+    backgroundColor: '#6A1B4D',
+    paddingVertical: 10,
+    paddingHorizontal: 40
+  },
+  actionBtnTxt: {
+    fontSize: 24,
+    color: '#FFFFFF',
+    textTransform: 'uppercase'
+  },
+})
