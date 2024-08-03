7ebf4b76e95a9973b2e24c183a2cd74dda010a78
TBS96 <prantik.g1996@gmail.com>
2024-08-03 21:55:17 +0530

    create own module in index.d.ts for png files & import them in App.tsx
    Signed-off-by: TBS96 <prantik.g1996@gmail.com>

--- ReactNativeApp07/src/App.tsx
+++ ReactNativeApp07/src/App.tsx
@@ -1,5 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import DiceOne from '../assets/One.png'
+import DiceTwo from '../assets/Two.png'
+import DiceThree from '../assets/Three.png'
+import DiceFour from '../assets/Four.png'
+import DiceFive from '../assets/Five.png'
+import Dicesix from '../assets/Six.png'
 
 const App = () => {
   return (
@@ -11,4 +17,29 @@ const App = () => {
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF2F2',
+  },
+  diceContainer: {
+    margin: 12,
+  },
+  diceImage: {
+    width: 200,
+    height: 200,
+  },
+  rollDiceBtnText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#E5E0FF',
+    fontSize: 16,
+    color: '#8EA7E9',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+  }
+})
