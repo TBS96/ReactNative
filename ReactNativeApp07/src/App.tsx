e64287c6dffb21cb188ffe327d4d1cab129a833d
TBS96 <prantik.g1996@gmail.com>
2024-08-03 22:12:14 +0530

    create type DiceProps for img & create a fn Dice in which it will return Image => Part: 1
    Signed-off-by: TBS96 <prantik.g1996@gmail.com>

--- ReactNativeApp07/src/App.tsx
+++ ReactNativeApp07/src/App.tsx
@@ -1,11 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, ImageSourcePropType, StyleSheet, Text, View } from 'react-native'
+import React, { PropsWithChildren } from 'react'
 import DiceOne from '../assets/One.png'
 import DiceTwo from '../assets/Two.png'
 import DiceThree from '../assets/Three.png'
 import DiceFour from '../assets/Four.png'
 import DiceFive from '../assets/Five.png'
 import Dicesix from '../assets/Six.png'
+
+type DiceProps = PropsWithChildren<{
+  imageUrl: ImageSourcePropType
+}>
+
+const Dice = ({imageUrl}: DiceProps) => {
+  return(
+    <View>
+      <Image style={styles.diceImage} source={imageUrl} />
+    </View>
+  )
+};
 
 const App = () => {
   return (
