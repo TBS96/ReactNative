e4b23595673ad6f2a6792bfe4c9838b2f326a770
TBS96 <prantik.g1996@gmail.com>
2024-08-07 19:49:33 +0530

    create component => Icons.tsx and add icons for tictac toe using switch case & also import the Icon from 'react-native-vector-icons/FontAwesome
    Signed-off-by: TBS96 <prantik.g1996@gmail.com>

---
+++ ReactNativeApp09/src/components/Icons.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import type { PropsWithChildren } from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+type IconsProps = PropsWithChildren<{
+    name: string
+}>
+
+
+const Icons = ({name}: IconsProps) => {
+  switch (name) {
+    case 'circle':
+        return <Icon name="circle-thin" size={38} color="green" />
+        break;
+    case 'cross':
+        return <Icon name="times" size={38} color="red" />
+        break;
+  
+    default:
+      return <Icon name="pencil" size={38} color="orange" />
+  }
+}
+
+const styles = StyleSheet.create({})
+
+export default Icons
