abd43e3b50d52682a5ae9e9ef73497b85cee60d6
TBS96 <prantik.g1996@gmail.com>
2024-08-09 22:15:53 +0530

    create 'musicPlayerServices.js' & add fns. using async-await: setupPlayer(), addTrack(), playbackService()
    Signed-off-by: TBS96 <prantik.g1996@gmail.com>

--- ReactNativeApp10/musicPlayerServices.js
+++ ReactNativeApp10/musicPlayerServices.js
@@ -1,4 +1,43 @@
-import TrackPlayer from "react-native-track-player";
+import TrackPlayer, { Event, RepeatMode } from "react-native-track-player";
 import { playListData } from "./src/contants";
 
-export const playbackService = async() => {};
+export const setupPlayer = async() => {
+    let isSetup = false;
+    try
+    {
+        await TrackPlayer.getActiveTrackIndex();
+        isSetup = true;
+    }
+    catch(error)
+    {
+        await TrackPlayer.setupPlayer();
+        isSetup = true;
+    }
+    finally
+    {
+        return isSetup;
+    }
+};
+
+export const addTrack = async() => {
+    await TrackPlayer.add(playListData);
+    await TrackPlayer.setRepeatMode(RepeatMode.Queue);
+};
+
+export const playbackService = async() => {
+    TrackPlayer.addEventListener(Event.RemotePause, () => {
+        TrackPlayer.pause()
+    })
+
+    TrackPlayer.addEventListener(Event.RemotePlay, () => {
+        TrackPlayer.play()
+    })
+
+    TrackPlayer.addEventListener(Event.RemoteNext, () => {
+        TrackPlayer.skipToNext()
+    })
+
+    TrackPlayer.addEventListener(Event.RemotePrevious, () => {
+        TrackPlayer.skipToPrevious()
+    })
+};
