7c34384eb14b0c987fde7572fae6243e2976124c
TBS96 <prantik.g1996@gmail.com>
2024-08-09 22:56:54 +0530

    in App.tsx, call musicPlayerServices ; useState(), useEffect() ; basic setup on rendering part: intro to <ActivityIndicator />
    Signed-off-by: TBS96 <prantik.g1996@gmail.com>

--- ReactNativeApp10/src/App.tsx
+++ ReactNativeApp10/src/App.tsx
@@ -1,14 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ActivityIndicator, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { setupPlayer, addTrack } from '../musicPlayerServices'
 
 const App = () => {
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
+
+  const setup = async() => {
+    let isSetup = await setupPlayer();
+    if(isSetup)
+    {
+      await addTrack();
+    }
+    setIsPlayerReady(isSetup);
+  };
+
+  useEffect(() => {
+    setup();
+  }, [])
+
+  // is the player is not ready, render below code using <ActivityIndicator>. Its just a loading screen like youtube buffering
+  if(!isPlayerReady)
+  {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    )
+  }
+  
   return (
-    <View>
+    <SafeAreaView>
+      <StatusBar />
       <Text>App</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
