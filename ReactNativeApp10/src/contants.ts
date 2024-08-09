9d03e2d21affbba3ba689aa68526ee9f86d59ee9
TBS96 <prantik.g1996@gmail.com>
2024-08-09 20:59:49 +0530

    created file constants.ts from where we are exporting an array of objects that contains metadata of audio files ; import { Track } from 'react-native-track-player' npm pkg
    Signed-off-by: TBS96 <prantik.g1996@gmail.com>

---
+++ ReactNativeApp10/src/contants.ts
@@ -0,0 +1,52 @@
+import { Track } from "react-native-track-player";
+
+export const playListData : Track[] = [
+    {
+        id: 1,
+        title: 'EIDA - Aurora Dreams (Official Audio)',
+        artist: 'M. Samiul Haque | Raihan Mahbub Rasha | Sakib Manzur Zihan | Hassan Munhamanna | Arjo Biswas',
+        album: 'Pop Songs for No One | EIDA',
+        artwork: 'https://e-cdn-images.dzcdn.net/images/cover/5b5c3506a91711e18290aad651743c45/500x500-000000-80-0-0.jpg',
+        url: require('./assets/audio/EIDA-AuroraDreams.mp3')
+    },
+    {
+        id: 2,
+        title: 'EIDA - Find Me (Official Audio)',
+        artist: 'M. Samiul Haque | Raihan Mahbub Rasha | Sakib Manzur Zihan | Hassan Munhamanna | Arjo Biswas',
+        album: 'Pop Songs for No One | EIDA',
+        artwork: require('./assets/artwork/findme.jpg'),
+        url: require('./assets/audio/EIDA-FindMe.mp3')
+    },
+    {
+        id: 3,
+        title: 'EIDA - In the Blind (Official Audio)',
+        artist: 'M. Samiul Haque | Raihan Mahbub Rasha | Sakib Manzur Zihan | Hassan Munhamanna | Arjo Biswas',
+        album: 'Pop Songs for No One | EIDA',
+        artwork: 'https://is1-ssl.mzstatic.com/image/thumb/Music113/v4/2f/8a/ad/2f8aadd1-25a9-ed63-1604-f86618568f79/artwork.jpg/316x316bb.webp',
+        url: require('./assets/audio/EIDA-InTheBlind.mp3')
+    },
+    {
+        id: 4,
+        title: 'EIDA - Nightdriver (Official Audio)',
+        artist: 'M. Samiul Haque | Raihan Mahbub Rasha | Sakib Manzur Zihan | Hassan Munhamanna | Arjo Biswas',
+        album: 'Pop Songs for No One | EIDA',
+        artwork: 'https://i.scdn.co/image/ab67616d00001e02b4b49009fd7f37ac351ab525',
+        url: require('./assets/audio/EIDA-Nightdriver.mp3')
+    },
+    {
+        id: 5,
+        title: 'EIDA - Relentless (Official Audio)',
+        artist: 'M. Samiul Haque | Raihan Mahbub Rasha | Sakib Manzur Zihan | Hassan Munhamanna | Arjo Biswas',
+        album: 'Pop Songs for No One | EIDA',
+        artwork: 'https://i.scdn.co/image/ab67616d00001e020b073900657f89121d5f54de',
+        url: require('./assets/audio/EIDA-Relentless.mp3')
+    },
+    {
+        id: 6,
+        title: 'EIDA - What It Means (Official Audio)',
+        artist: 'M. Samiul Haque | Raihan Mahbub Rasha | Sakib Manzur Zihan | Hassan Munhamanna | Arjo Biswas',
+        album: 'Pop Songs for No One | EIDA',
+        artwork: 'https://i.scdn.co/image/ab67616d00001e02305a2ff5f27b4a2d16327126',
+        url: require('./assets/audio/EIDA-WhatItMeans.mp3')
+    },
+];
