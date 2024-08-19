0dea6c83d681b8efed69914029f8e9b32f764702
TBS96 <9tbs6@proton.me>
2024-08-19 19:36:42 +0530

    import { NavigationContainer } from '@react-navigation/native'; & import { createNativeStackNavigator } from '@react-navigation/native-stack';
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- ReactNativeApp11_1/src/App.tsx
+++ ReactNativeApp11_1/src/App.tsx
@@ -2,8 +2,8 @@ import {Text, View } from 'react-native'
 import React from 'react'
 
 // Navigation
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Screens
 import Home from './screens/Home'
