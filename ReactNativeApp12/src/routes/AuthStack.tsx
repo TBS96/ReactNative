8dbca3c86ec4a6ee5f8d577b87efd426ac757368
TBS96 <9tbs6@proton.me>
2024-08-22 21:06:35 +0530

    create AuthStack and import Login & Signup screens ; Stack=createNativeStackNavigator()
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- ReactNativeApp12/src/routes/AuthStack.tsx
+++ ReactNativeApp12/src/routes/AuthStack.tsx
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Signup from '../screens/Signup';
+import Login from '../screens/Login';
+
+
+export type AuthStackParamList = {
+    Signup: undefined;
+    Login: undefined;
+}
+
+const Stack = createNativeStackNavigator<AuthStackParamList>();
 
 const AuthStack = () => {
   return (
-    <View>
-      <Text>AuthStack</Text>
-    </View>
+    <Stack.Navigator screenOptions={{
+        headerTitleAlign: 'center',
+        headerBackTitleVisible: false
+    }}>
+      <Stack.Screen name='Login' component={Login} />
+      <Stack.Screen name='Signup' component={Signup} />
+    </Stack.Navigator>
   )
 }
 
 export default AuthStack
-
-const styles = StyleSheet.create({})
