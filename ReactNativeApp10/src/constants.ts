1b337c7259dd878758f69294bfb85f98eb91f682
TBS96 <9tbs6@proton.me>
2024-08-27 21:32:22 +0530

    added bolly. tracks
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- ReactNativeApp10/src/constants.ts
+++ ReactNativeApp10/src/constants.ts
@@ -3,6 +3,214 @@ import { Track } from "react-native-track-player";
 export const playListData : Track[] = [
     {
         id: 1,
+        title: 'Allah Hafiz',
+        artist: 'Kiran Kamath | KK',
+        album: 'Bhool Bhulaiyaa',
+        artwork: 'https://c.saavncdn.com/056/Bhool-Bhulaiyaa-Hindi-2007-20221122005742-500x500.jpg',
+        url: require('./assets/audio/AllahHafiz.mp3')
+    },
+    {
+        id: 2,
+        title: 'Bheegi Bheegi',
+        artist: ' James | Pritam Chakraborty',
+        album: 'Gangster',
+        artwork: 'https://c.saavncdn.com/810/Gangster-Hindi-2024-20240330053536-500x500.jpg',
+        url: require('./assets/audio/BheegiBheegi.mp3')
+    },
+    {
+        id: 3,
+        title: 'Chand Sifarish',
+        artist: 'Shaan',
+        album: 'Fanaa',
+        artwork: 'https://c.saavncdn.com/146/Fanaa-Hindi-2006-20190329181154-500x500.jpg',
+        url: require('./assets/audio/ChandSifarish.mp3')
+    },
+    {
+        id: 4,
+        title: 'Dus Bahane',
+        artist: 'Sunidhi Chauhan | Sonu Nigam | KK | Shaan | Babul Supriyo',
+        album: 'Dus',
+        artwork: 'https://c.saavncdn.com/759/Dus-Hindi-2005-20221124183214-500x500.jpg',
+        url: require('./assets/audio/DusBahane.mp3')
+    },
+    {
+        id: 5,
+        title: 'Gunaah',
+        artist: 'Jeet Gannguli | Mustafa Zahid',
+        album: 'Blood Money',
+        artwork: 'https://c.saavncdn.com/886/Blood-Money-Hindi-2012-20230306184731-500x500.jpg',
+        url: require('./assets/audio/Gunaah.mp3')
+    },
+    {
+        id: 6,
+        title: 'Gunaah - Unplugged',
+        artist: 'Jeet Gannguli | Rana Mazumder',
+        album: 'Blood Money',
+        artwork: 'https://c.saavncdn.com/886/Blood-Money-Hindi-2012-20230306184731-500x500.jpg',
+        url: require('./assets/audio/GunaahUnplugged.mp3')
+    },
+    {
+        id: 7,
+        title: 'Judai',
+        artist: 'Pritam | Kamran Ahmad',
+        album: 'Jannat',
+        artwork: 'https://c.saavncdn.com/801/Jannat-Hindi-2008-20190629135803-500x500.jpg',
+        url: require('./assets/audio/JudaiKamranAhmad.mp3')
+    },
+    {
+        id: 8,
+        title: 'Judai',
+        artist: 'Pritam | Richaa Sharma',
+        album: 'Jannat',
+        artwork: 'https://c.saavncdn.com/801/Jannat-Hindi-2008-20190629135803-500x500.jpg',
+        url: require('./assets/audio/JudaiRichaaSharma.mp3')
+    },
+    {
+        id: 9,
+        title: 'Khwabon Khwabon',
+        artist: ' Harris Jayaraj | KK | Suchitra',
+        album: 'Force',
+        artwork: 'https://c.saavncdn.com/002/Force-Hindi-2011-20221212001455-500x500.jpg',
+        url: require('./assets/audio/KhwabonKhwabon.mp3')
+    },
+    {
+        id: 10,
+        title: 'Kya Mujhe Pyaar Hai',
+        artist: 'Kiran Kamath | KK',
+        album: 'Woh Lamhe',
+        artwork: 'https://c.saavncdn.com/832/Woh-Lamhe-Hindi-2006-20220413121014-500x500.jpg',
+        url: require('./assets/audio/KyaMujhePyaarHai.mp3')
+    },
+    {
+        id: 11,
+        title: 'Kya Mujhe Pyaar Hai - Remix',
+        artist: 'Kiran Kamath | KK',
+        album: 'Woh Lamhe',
+        artwork: 'https://c.saavncdn.com/832/Woh-Lamhe-Hindi-2006-20220413121014-500x500.jpg',
+        url: require('./assets/audio/KyaMujhePyarHaiRemix.mp3')
+    },
+    {
+        id: 12,
+        title: 'Labon Ko',
+        artist: 'Kiran Kamath | KK',
+        album: 'Bhool Bhulaiyaa',
+        artwork: 'https://c.saavncdn.com/056/Bhool-Bhulaiyaa-Hindi-2007-20221122005742-500x500.jpg',
+        url: require('./assets/audio/LabonKo.mp3')
+    },
+    {
+        id: 13,
+        title: 'Mahiya',
+        artist: "Suzzane D'mello",
+        album: 'Awarapan',
+        artwork: 'https://c.saavncdn.com/803/Awarapan-Original-Soundtrack--Hindi-2022-20220321144734-500x500.jpg',
+        url: require('./assets/audio/Mahiya.mp3')
+    },
+    {
+        id: 14,
+        title: 'Mere Haath Mein',
+        artist: "Jatin-Lalit | Sunidhi Chauhan | Sonu Nigam | Prasoon Joshi",
+        album: 'Fanaa',
+        artwork: 'https://c.saavncdn.com/146/Fanaa-Hindi-2006-20190329181154-500x500.jpg',
+        url: require('./assets/audio/MereHaathMein.mp3')
+    },
+    {
+        id: 15,
+        title: 'Tera Mera Rishta',
+        artist: 'Mustafa Zahid',
+        album: 'Awarapan',
+        artwork: 'https://c.saavncdn.com/803/Awarapan-Original-Soundtrack--Hindi-2022-20220321144734-500x500.jpg',
+        url: require('./assets/audio/TeraMeraRishta.mp3')
+    },
+    {
+        id: 16,
+        title: 'Teri Yaadon Se',
+        artist: 'Pranay M. Rijia | Mustafa Zahid',
+        album: 'Blood Money',
+        artwork: 'https://c.saavncdn.com/886/Blood-Money-Hindi-2012-20230306184731-500x500.jpg',
+        url: require('./assets/audio/TeriYaadonSe.mp3')
+    },
+    {
+        id: 17,
+        title: 'Teri Yaadon Se - Remix',
+        artist: 'KK | Mustafa Zahid',
+        album: 'Blood Money',
+        artwork: 'https://c.saavncdn.com/886/Blood-Money-Hindi-2012-20230306184731-500x500.jpg',
+        url: require('./assets/audio/TeriYaadonSeRemixKiranKamath.mp3')
+    },
+    {
+        id: 18,
+        title: 'Toh Phir Aao',
+        artist: 'Mustafa Zahid',
+        album: 'Awarapan',
+        artwork: 'https://c.saavncdn.com/803/Awarapan-Original-Soundtrack--Hindi-2022-20220321144734-500x500.jpg',
+        url: require('./assets/audio/TohPhirAao.mp3')
+    },
+    {
+        id: 19,
+        title: 'Tu Hai Ki Nahi',
+        artist: 'Ankit Tiwari',
+        album: 'Roy',
+        artwork: 'https://c.saavncdn.com/647/Roy-Hindi-2015-500x500.jpg',
+        url: require('./assets/audio/TuHaiKiNahi.mp3')
+    },
+    {
+        id: 20,
+        title: 'Tu Hai Ki Nahi - Unplugged',
+        artist: 'Tulsi Kumar',
+        album: 'Roy',
+        artwork: 'https://c.saavncdn.com/647/Roy-Hindi-2015-500x500.jpg',
+        url: require('./assets/audio/TuHaiKiNahiUnplugged.mp3')
+    },
+    {
+        id: 21,
+        title: 'Tu Hi Meri Shab Hai',
+        artist: 'Pritam | KK',
+        album: 'Gangster',
+        artwork: 'https://c.saavncdn.com/810/Gangster-Hindi-2024-20240330053536-500x500.jpg',
+        url: require('./assets/audio/TuHiMeriShabHai.mp3')
+    },
+    {
+        id: 22,
+        title: 'Ya Ali',
+        artist: 'Pritam | Zubeen Garg',
+        album: 'Gangster',
+        artwork: 'https://c.saavncdn.com/810/Gangster-Hindi-2024-20240330053536-500x500.jpg',
+        url: require('./assets/audio/YaAliGangster.mp3')
+    },
+    {
+        id: 23,
+        title: 'Zara Sa',
+        artist: 'Pritam | KK',
+        album: 'Jannat',
+        artwork: 'https://c.saavncdn.com/801/Jannat-Hindi-2008-20190629135803-500x500.jpg',
+        url: require('./assets/audio/ZaraSaJannat.mp3')
+    },
+    {
+        id: 24,
+        title: 'Jo Tere Sang',
+        artist: 'Jeet Gannguli | Mustafa Zahid',
+        album: 'Blood Money',
+        artwork: 'https://c.saavncdn.com/886/Blood-Money-Hindi-2012-20230306184731-500x500.jpg',
+        url: require('./assets/audio/JoTereSang.mp3')
+    },
+    {
+        id: 25,
+        title: 'Hum Jee Lenge',
+        artist: 'Mustafa Zahid',
+        album: 'Murder 3',
+        artwork: 'https://c.saavncdn.com/513/Murder-3-2013-500x500.jpg',
+        url: require('./assets/audio/HumJeeLenge.mp3')
+    },
+    {
+        id: 26,
+        title: 'Hum Jee Lenge - Rock Version',
+        artist: 'Mustafa Zahid | Roxen',
+        album: 'Murder 3',
+        artwork: 'https://c.saavncdn.com/513/Murder-3-2013-500x500.jpg',
+        url: require('./assets/audio/HumJeeLengeRock.mp3')
+    },
+    {
+        id: 27,
         title: 'EIDA - Aurora Dreams (Official Audio)',
         artist: 'M. Samiul Haque | Raihan Mahbub Rasha | Sakib Manzur Zihan | Hassan Munhamanna | Arjo Biswas',
         album: 'Pop Songs for No One | EIDA',
@@ -10,7 +218,7 @@ export const playListData : Track[] = [
         url: require('./assets/audio/EIDA-AuroraDreams.mp3')
     },
     {
-        id: 2,
+        id: 28,
         title: 'EIDA - Find Me (Official Audio)',
         artist: 'M. Samiul Haque | Raihan Mahbub Rasha | Sakib Manzur Zihan | Hassan Munhamanna | Arjo Biswas',
         album: 'Pop Songs for No One | EIDA',
@@ -18,7 +226,7 @@ export const playListData : Track[] = [
         url: require('./assets/audio/EIDA-FindMe.mp3')
     },
     {
-        id: 3,
+        id: 29,
         title: 'EIDA - In the Blind (Official Audio)',
         artist: 'M. Samiul Haque | Raihan Mahbub Rasha | Sakib Manzur Zihan | Hassan Munhamanna | Arjo Biswas',
         album: 'Pop Songs for No One | EIDA',
@@ -26,7 +234,7 @@ export const playListData : Track[] = [
         url: require('./assets/audio/EIDA-InTheBlind.mp3')
     },
     {
-        id: 4,
+        id: 30,
         title: 'EIDA - Nightdriver (Official Audio)',
         artist: 'M. Samiul Haque | Raihan Mahbub Rasha | Sakib Manzur Zihan | Hassan Munhamanna | Arjo Biswas',
         album: 'Pop Songs for No One | EIDA',
@@ -34,7 +242,7 @@ export const playListData : Track[] = [
         url: require('./assets/audio/EIDA-Nightdriver.mp3')
     },
     {
-        id: 5,
+        id: 31,
         title: 'EIDA - Relentless (Official Audio)',
         artist: 'M. Samiul Haque | Raihan Mahbub Rasha | Sakib Manzur Zihan | Hassan Munhamanna | Arjo Biswas',
         album: 'Pop Songs for No One | EIDA',
@@ -42,7 +250,7 @@ export const playListData : Track[] = [
         url: require('./assets/audio/EIDA-Relentless.mp3')
     },
     {
-        id: 6,
+        id: 32,
         title: 'EIDA - What It Means (Official Audio)',
         artist: 'M. Samiul Haque | Raihan Mahbub Rasha | Sakib Manzur Zihan | Hassan Munhamanna | Arjo Biswas',
         album: 'Pop Songs for No One | EIDA',
